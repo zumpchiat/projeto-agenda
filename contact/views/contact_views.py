3a03662505bdcf3f80ed91aacb7851dc59a22b70
zumpchiat <zumpchiat@gmail.com>
2024-08-20 11:02:20 -0300

    :construction: feat: pagination

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.core.paginator import Paginator
 from django.db.models import Q
 
 def index(request):
     contacts = Contact.objects \
         .all() \
         .filter(show=True)\
-        .order_by('-id')[:12]
+        .order_by('-id')
+    
+    paginator = Paginator(contacts, 8)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
                                                                  
@@ -36,9 +42,15 @@ def search(request):
             )\
         .order_by('-id')
     
+    paginator = Paginator(contacts, 8)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
    
     context = {
-        'contacts': contacts,
+        #'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
                                                                  

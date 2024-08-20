abc9d95cf6789fba9a73ccd3e0981c47a9270c0f
zumpchiat <zumpchiat@gmail.com>
2024-08-20 11:02:48 -0300

    :construction: feat: contact url detail

--- contact/urls.py
+++ contact/urls.py
@@ -6,7 +6,14 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:id>/', views.contact, name='contact'),
+    
+    
     path('search/', views.search, name='search'),
+    path('', views.index, name='index'),
+
+    #Contact
+    path('contact/<int:id>/detail/', views.contact, name='contact'),
+    #path('contact/create/', views.contact, name='create'),
+    #path('contact/<int:id>/update/', views.contact, name='update'),
+    #path('contact/<int:id>/delete/', views.contact, name='delete'),
 ]

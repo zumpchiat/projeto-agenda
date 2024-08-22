d5d353e76318f451059be22aaf4e29d64c5f56fd
zumpchiat <zumpchiat@gmail.com>
2024-08-22 16:44:58 -0300

    :construction: feat: contactForm in forms

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,26 +1,6 @@
 from django.shortcuts import render
+from contact.forms import ContactForm
 
-from contact.models import Contact
-
-from django import forms
-from django.core.exceptions import ValidationError
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = 'first_name', 'last_name', 'phone', 'email',
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-
-        )
 
 def create(request):
     
